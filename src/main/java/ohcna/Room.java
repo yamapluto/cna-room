8f3a43b104545eb15c5dd56eaafe35568922fafa
김태곤(KIM TAEGON)/Digital Process혁신담당/SK <06711@skcc.com>
2020-09-02 20:05:27 +0900

    4th commit

--- src/main/java/ohcna/Room.java
+++ src/main/java/ohcna/Room.java
@@ -16,21 +16,29 @@ public class Room {
 
     @PostPersist
     public void onPostPersist(){
+
         RoomCreated roomCreated = new RoomCreated();
         BeanUtils.copyProperties(this, roomCreated);
         roomCreated.publishAfterCommit();
 
         //Following code causes dependency to external APIs
         // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
-
+        String chgDtm = "20200902"+(100000 + Math.random() * 1);
         ohcna.external.Point point = new ohcna.external.Point();
         // mappings goes here
         point.setId(roomCreated.getId());
         point.setStatus("created");
+        point.setChangeDtm(chgDtm);
         point.setPoint(0);
-        System.out.println("##### point onPostPersist : " + point.getId()+"<<<<<<<");
+        System.out.println("##### point onPostPersist ID: " + point.getId()+"<<<<<<<");
+        System.out.println("##### point onPostPersist Status: " + point.getStatus()+"<<<<<<<");
+        System.out.println("##### point onPostPersist Point: " + point.getPoint()+"<<<<<<<");
+        System.out.println("##### point onPostPersist ChangeDtm: " + point.getChangeDtm()+"<<<<<<<");
         RoomApplication.applicationContext.getBean(ohcna.external.PointService.class)
             .pointCreate(point);
+
+
+
 
 
     }
